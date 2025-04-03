8b6a9240f5783ded90eacf7de03dcf3cf808e534
Sergio Monsalve <smonsalve@godaddy.com>
2025-04-03 14:42:07 -0500

    tests

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,6 +24,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
     from . import db
     db.init_app(app)
 
@@ -33,13 +38,5 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
-
-
-    return app
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
 
     return app
